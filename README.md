7b925b7a01d5bd7e2d2d65461db3dcfc8f45d2e6
Kyriacos Elia <kyriacos.elia@ubcmedia.com>
2014-11-21 10:48:01 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,66 @@
+About ElasticJack
+===
+
+ElasticJack is a Python script built on top of [Boto](https://github.com/boto/boto) and 
+[Shovel](https://github.com/seomoz/shovel) to allow easy deployment of applications onto AWS Elastic Beanstalk. 
+The script uploads a deployable (e.g. a war file) to Amazon S3 and then creates/updates the environment for the 
+application based on a given YAML configuration.
+
+Setup
+===
+
+Install dependencies
+---
+
+To install the required python dependencies, run the following command:
+
+```pip install -r requirements.txt```
+
+If you do not have Pip installed, read more about [installing Pip here](http://pip.readthedocs.org/en/latest/installing.html).
+
+AWS Access Config
+---
+
+For the deployer to work, you need to create a file on your system to store your AWS credentials.
+
+In Unix/Linux store your AWS Access Key ID and AWS Secret Access Key in a file ~/.aws/credentials in the 
+following format:
+
+```
+[Credentials]
+aws_access_key_id = <your_access_key_here>
+aws_secret_access_key = <your_secret_key_here>
+```
+
+In Windows, create a text file that has any name (e.g. boto.config). It’s recommended that you put this 
+file in your user folder. Then set a user environment variable named BOTO_CONFIG to the full path of that 
+file.
+
+For more information, read about how [Boto manages AWS credentials](http://boto.readthedocs.org/en/latest/boto_config_tut.html).
+
+Create a YAML for an application
+---
+
+Each app should have its own YAML file containing the S3 and EBS configurations needed to deploy the app
+onto AWS. The following settings should be present:
+
+*	appName
+*	region (e.g. eu-west-1)
+*	s3Bucket
+*	s3Key
+*	versionLabel
+*	environmentName
+*	cName
+*	configTemplate
+
+The yaml file can also include a keyword '$now'. The deployer will automatically replace any mentions of 
+'$now' with the timestamp that the deploy script was ran at.
+
+E.g. If ran at 31/12/14 23:59:59, 's3Key: deploy-$now.war' becomes 's3Key: deploy-20143112235959.war'
+
+Using
+===
+
+To upload a file to AWS, run the following command:
+
+```shovel deploy <yamlFilePath> <deployableFilePath>```
---
+++ shovel.py
@@ -0,0 +1,102 @@
+import boto
+import boto.beanstalk
+import sys
+import time
+import yaml
+from datetime import datetime
+from shovel import task
+
+@task
+def deploy(yaml_path, war_path):
+    settings = load_yaml(yaml_path)
+    app_name = settings['appName']
+    region = settings['region']
+    s3_bucket = settings['s3Bucket']
+    s3_key = settings['s3Key']
+    version_label = settings['versionLabel']
+    env_name = settings['environmentName']
+    c_name = settings['cName']
+    config_template = settings['configTemplate']
+
+    eb_client = boto.beanstalk.connect_to_region(region)
+
+    upload_to_s3(war_path, s3_bucket, s3_key, region)
+    if application_exists(eb_client, c_name):
+        print "Application '%s' already exists - attempting update" % app_name
+        deploy_app(eb_client, app_name, version_label, s3_bucket, s3_key, False)
+        update_app(eb_client, env_name, version_label, config_template)
+    else:
+        print "Could not find application '%s' - creating application" % app_name
+        deploy_app(eb_client, app_name, version_label, s3_bucket, s3_key, True)
+        create_env(eb_client, app_name, env_name, c_name, version_label, config_template)
+
+
+def load_yaml(yaml_path):
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    file = open(yaml_path, "r")
+    doc = str(yaml.load(file))
+    doc = doc.replace('$now', now)
+    return yaml.load(doc)
+
+def upload_to_s3(file, bucket_name, key_name, region):
+    print "Uploading file %s to Amazon S3 bucket '%s'" % (file, bucket_name)
+    s3 = boto.s3.connect_to_region(region)
+    bucket = get_or_create_bucket(s3, bucket_name)
+    key = boto.s3.key.Key(bucket)
+    key.key = key_name
+    key.set_contents_from_filename(file, cb=percent_complete, num_cb=10)
+
+def get_or_create_bucket(s3, bucket_name):
+    bucket = s3.lookup(bucket_name)
+    if bucket is None:
+        bucket = s3.create_bucket(bucket_name)
+    return bucket
+
+def application_exists(eb_client, c_name):
+    response = eb_client.check_dns_availability(c_name)
+    available = response['CheckDNSAvailabilityResponse']['CheckDNSAvailabilityResult']['Available']
+    return not available
+
+def deploy_app(eb_client, app_name, version_label, s3_bucket, s3_key, auto_create):
+    print "Creating application version '%s'" % version_label
+    eb_client.create_application_version(
+        app_name,
+        version_label,
+        s3_bucket=s3_bucket,
+        s3_key=s3_key,
+        auto_create_application=auto_create)
+
+def create_env(eb_client, app_name, env_name, c_name, version_label, template_name):
+    print "Creating environment '%s'" % env_name
+    eb_client.create_environment(
+        app_name,
+        env_name,
+        cname_prefix=c_name,
+        version_label=version_label,
+        template_name=template_name)
+
+def update_app(eb_client, env_name, version_label, template_name):
+    print "Updating environment '%s' with version '%s'" % (env_name, version_label)
+    eb_client.update_environment(
+        environment_name=env_name,
+        version_label=version_label,
+        template_name=template_name)
+
+def wait_for_app(eb_client, app_name, version_label):
+    print "Waiting for application to start"
+    status = 'Pending'
+    while status == 'Pending':
+        print "..."
+        time.sleep(5)
+        response = eb_client.describe_environments(application_name=app_name, version_label=version_label)
+        env = response['DescribeEnvironmentsResponse']['DescribeEnvironmentsResult']['Environments'][0]
+        status = env['Status']
+    if status == 'Ready':
+        print "Application is ready"
+    else:
+        print "WARNING - application has status %s" % status
+
+# The callback function in boto requires there to be two params
+def percent_complete(complete, total):
+    sys.stdout.write(".")
+    sys.stdout.flush()
